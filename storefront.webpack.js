4ef5fc6bf7c1811a6cfb10409a84df347524b4a8
matheusgnreis <matheus@e-com.club>
2023-06-16 00:29:31 -0300

    chore: update summary

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -8,7 +8,8 @@ module.exports = () => ({
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
       './js/PaymentOption.js': path.resolve(__dirname, 'template/js/custom-js/js/PaymentOption.js'),
       './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/js/APrices.js'),
-      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html')
+      './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html'),
+      './html/EcSummary.html': path.resolve(__dirname, 'template/js/custom-js/html/EcSummary.html')
     }
   }
 })
